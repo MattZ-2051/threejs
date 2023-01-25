e58c0bcdad2b0d6334c28831144485fea1c6e50b
Matt Zamora <mzamora2899@gmail.com>
2023-01-25 12:20:20 -0700

    3d text r3 notes

---
+++ threejs-journey/chapter-7/47-3d-text-with-r3f/src/Experience.jsx
@@ -0,0 +1,40 @@
+import {
+  OrbitControls,
+  Text3D,
+  Center,
+  useMatcapTexture,
+} from '@react-three/drei';
+import { Perf } from 'r3f-perf';
+
+export default function Experience() {
+  const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
+
+  return (
+    <>
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+      <Center>
+        <Text3D
+          font="./fonts/helvetiker_regular.typeface.json"
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
+          Some Text
+          <meshMatcapMaterial matcap={matcapTexture} />
+        </Text3D>
+      </Center>
+
+      <mesh>
+        <torusGeometry />
+        <meshMatcapMaterial matcap={matcapTexture} />
+      </mesh>
+    </>
+  );
+}
